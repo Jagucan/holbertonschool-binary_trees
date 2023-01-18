ee0b95600926375c3da97d83623e6f342186dcaa
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-01-17 20:50:48 -0500

    Depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,8 +9,12 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth = 0;
 
+	if (!tree)
+		return (0);
+
 	if (!tree->parent)
 		return (0);
+
 	depth = binary_tree_depth(tree->parent);
 	return (depth + 1);
 }
