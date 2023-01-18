3d4068e021f9a6184c7955471565d08823226450
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-01-18 12:39:00 -0500

    Balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -21,7 +21,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	
 	return (1 + right);
 }
-
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree.
  * @tree: Is a pointer to the root node.
