3bc36bc0e4b18aa389cff5c997c16f11262da62d
CAMILO <cestrad5@gmail.com>
2023-01-18 12:28:34 -0500

    Error solved 3

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,41 +8,29 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t l_height = 0, r_height = 0;
+	size_t left, right;
 
-	if (!tree)
+	if (tree == NULL)
 		return (0);
-
-	if (!tree->left && !tree->right)
-		return (0);
-
-	l_height = binary_tree_height(tree->left);
-	r_height = binary_tree_height(tree->right);
-
-	if (l_height >= r_height)
-		return (l_height + 1);
-
-	return (r_height + 1);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	if (left >= right)
+		return (1 + left);
+	return (1 + right);
 }
 
 /**
- * 
- * 
-*/
-
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node 
+ * Return: balance factor of a binary tree
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	/*int l_height = 0, r_height = 0;*/
+	int left, right;
 
-	if (!tree)
+	if (tree == NULL)
 		return (0);
-
-	/*if (tree->left)
-		l_height = binary_tree_height(tree->left);
-
-	if (tree->right)
-		r_height = binary_tree_height(tree->right);
-*/
-		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	return (left - right);
 }
-
