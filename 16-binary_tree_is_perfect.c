6bacc688e0f090b973ae76f69d583a02dd00e38e
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-01-19 07:46:56 -0500

    Is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -22,6 +22,24 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	return (1 + right);
 }
 /**
+ * binary_tree_balance - Measures the balance factor of a binary tree.
+ * @tree: Is a pointer to the root node.
+ * Return: The balance factor of a binary tree.
+*/
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (!tree)
+		return (0);
+
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+
+	return (left - right);
+}
+/**
  * binary_tree_is_perfect - Checks if a binary tree is perfect.
  * @tree: is a pointer to the root node.
  * Return: If tree is NULL, return 0.
@@ -29,22 +47,17 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left = 0, right = 0;
-
 	if (!tree)
-		return (0);
+        return (1);
 
-	if (tree->left)
-		left = binary_tree_height(tree->left);
-		
-	if (tree->right)
-		right = binary_tree_height(tree->right);
+    if (binary_tree_balance(tree) != 0)
+        return (0);
 
-	if (left != right)
-		return (0);
+    if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
+        return (0);
 
-	if (!binary_tree_is_perfect(tree->left) || !binary_tree_is_perfect(tree->right))
-		return (0);
+    if (!binary_tree_is_perfect(tree->left) || !binary_tree_is_perfect(tree->right))
+        return (0);
 
-	return(1);
+    return (1);
 }
