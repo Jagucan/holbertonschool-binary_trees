8df3bf18463b94168e3bf85ef58d52d0ac8c5232
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-01-18 20:22:20 -0500

    Is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -13,9 +13,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (!tree->left && !tree->right)
-		return (0);
-
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 
@@ -43,9 +40,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (left != right)
 		return (0);
 
-	if (!binary_tree_is_perfect(tree->left) || !binary_tree_is_perfect(tree->right))
-		return (0); 
+	if (!binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
+		return (0);
 
-	else
-		return (1);
+	if (binary_tree_is_perfect(tree->left) && !binary_tree_is_perfect(tree->right))
+		return (0);
+
+	return(1);
 }
