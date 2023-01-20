5687f584261e9a1e93aeed0daf5d0a5da480ed97
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-01-19 20:10:25 -0500

    Sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,11 +8,15 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (!node || !node->parent)
+	if (!node)
 		return (NULL);
 
-	if (node->parent->left != node)
+	else if (!node->parent)
+		return (NULL);
+
+	else if (node->parent->left != node)
 		return (node->parent->left);
 
-	return (node->parent->right);
+	else
+		return (node->parent->right);
 }
