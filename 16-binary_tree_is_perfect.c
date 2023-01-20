c5cafa2e432520caac621c1114f1d9afdfa6efe5
CAMILO <cestrad5@gmail.com>
2023-01-19 22:54:19 -0500

    Fixing the 16 task

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -10,14 +10,14 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+if (tree == NULL)
+	return (0);
 
-	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
-	{
-			if (binary_tree_is_perfect(tree->left) == binary_tree_is_perfect(tree->right))
-			return (1);
-	}
+if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+{
+	if (binary_tree_is_perfect(tree->left) == binary_tree_is_perfect(tree->right))
+		return (1);
+}
 
 	return (0);
 }
